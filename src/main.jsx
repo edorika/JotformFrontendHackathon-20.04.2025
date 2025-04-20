fb13d48aaac1c3fd1ac9f14e074b3bcf78bc3716
Erdem Aydın <erdemaydin7139@gmail.com>
2025-04-20 15:42:32 +0300

    Add checkout page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
+import App from '@/App.jsx'
+import CheckoutPage from '@/pages/CheckoutPage.jsx' // make sure this file exists
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
