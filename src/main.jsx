613f96bece91d1e8b11accac6dc79d92ad47a79e
Erdem Aydın <erdemaydin7139@gmail.com>
2025-04-20 15:46:36 +0300

    Change pages structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
-import App from '@/App.jsx'
+import HomePage from '@/pages/HomePage.jsx'
 import CheckoutPage from '@/pages/CheckoutPage.jsx' // make sure this file exists
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
